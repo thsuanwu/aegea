39ac32bed2bdb7b1bb4cf86f509e81827a9c211a
Andrey Kislyuk <kislyuk@gmail.com>
2018-02-01 15:56:44 -0800

    Add aegea scp

--- aegea/ssh.py
+++ aegea/ssh.py
@@ -12,16 +12,17 @@ the prompt to save the instance public key and the resulting transient
 MITM vulnerability.
 """
 
-import os, sys, argparse, subprocess
+import os, sys, argparse, subprocess, string, functools
 
 from . import register_parser, logger
 from .util.aws import resolve_instance_id, resources, clients
 from .util.crypto import add_ssh_host_key_to_known_hosts
 from .util.printing import BOLD
 from .util.exceptions import AegeaException
+from .util.compat import lru_cache
 
-def ssh(args):
-    prefix, at, name = args.name.rpartition("@")
+@lru_cache(8)
+def resolve_instance_public_dns(name):
     instance = resources.ec2.Instance(resolve_instance_id(name))
     if not instance.public_dns_name:
         msg = "Unable to resolve public DNS name for {} (state: {})"
@@ -33,16 +34,40 @@ def ssh(args):
         # FIXME: this results in duplicates.
         # Use paramiko to detect if the key is already listed and not insert it then (or only insert if different)
         add_ssh_host_key_to_known_hosts(instance.public_dns_name + " " + ssh_host_key + "\n")
-    ssh_args = ["ssh", prefix + at + instance.public_dns_name]
+    return instance.public_dns_name
+
+def ssh(args):
+    prefix, at, name = args.name.rpartition("@")
+    ssh_args = ["ssh", prefix + at + resolve_instance_public_dns(name)]
     if not (prefix or at):
         try:
             ssh_args += ["-l", resources.iam.CurrentUser().user.name]
-        except:
+        except Exception:
             logger.info("Unable to determine IAM username, using local username")
     os.execvp("ssh", ssh_args + args.ssh_args)
 
-parser = register_parser(ssh, help="Connect to an EC2 instance", description=__doc__,
-                         formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument("name")
-parser.add_argument("ssh_args", nargs=argparse.REMAINDER,
-                    help="Arguments to pass to ssh; please see " + BOLD("man ssh") + " for details")
+ssh_parser = register_parser(ssh, help="Connect to an EC2 instance", description=__doc__,
+                             formatter_class=argparse.RawTextHelpFormatter)
+ssh_parser.add_argument("name")
+ssh_parser.add_argument("ssh_args", nargs=argparse.REMAINDER,
+                        help="Arguments to pass to ssh; please see " + BOLD("man ssh") + " for details")
+
+def scp(args):
+    user_or_hostname_chars = string.ascii_letters + string.digits
+    for i, arg in enumerate(args.scp_args):
+        if arg[0] in user_or_hostname_chars and ":" in arg:
+            hostname, colon, path = arg.partition(":")
+            username, at, hostname = hostname.rpartition("@")
+            hostname = resolve_instance_public_dns(hostname)
+            if not (username or at):
+                try:
+                    username, at = resources.iam.CurrentUser().user.name, "@"
+                except Exception:
+                    logger.info("Unable to determine IAM username, using local username")
+            args.scp_args[i] = username + at + hostname + colon + path
+    os.execvp("scp", ["scp"] + args.scp_args)
+
+scp_parser = register_parser(scp, help="Transfer files to or from EC2 instance", description=__doc__,
+                             formatter_class=argparse.RawTextHelpFormatter)
+scp_parser.add_argument("scp_args", nargs=argparse.REMAINDER,
+                        help="Arguments to pass to scp; please see " + BOLD("man scp") + " for details")
