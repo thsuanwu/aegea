855f00c1b8a2e25293bee30983589584d4e64626
Andrey Kislyuk <kislyuk@gmail.com>
2019-09-20 10:28:44 -0700

    Add aegea lambda update_config

--- aegea/lambda.py
+++ aegea/lambda.py
@@ -20,15 +20,16 @@ def ls(args):
     paginator = getattr(clients, "lambda").get_paginator("list_functions")
     page_output(tabulate(paginate(paginator), args, cell_transforms={"LastModified": Timestamp}))
 
-parser_ls = register_listing_parser(ls, parent=lambda_parser)
+parser_ls = register_listing_parser(ls, parent=lambda_parser, help="List AWS Lambda functions")
 
 def event_source_mappings(args):
     paginator = getattr(clients, "lambda").get_paginator("list_event_source_mappings")
     page_output(tabulate(paginate(paginator), args))
 
-parser_event_source_mappings = register_listing_parser(event_source_mappings, parent=lambda_parser)
+parser_event_source_mappings = register_listing_parser(event_source_mappings, parent=lambda_parser,
+                                                       help="List event source mappings")
 
-def update(args):
+def update_code(args):
     with open(args.zip_file, "rb") as fh:
         payload = fh.read()
     payload_sha = hashlib.sha256(payload).digest()
@@ -36,6 +37,33 @@ def update(args):
     assert base64.b64decode(res["CodeSha256"]) == payload_sha
     return res
 
-update_parser = register_parser(update, parent=lambda_parser)
-update_parser.add_argument("function_name")
-update_parser.add_argument("zip_file")
+update_code_parser = register_parser(update_code, parent=lambda_parser, help="Update function code")
+update_code_parser.add_argument("function_name")
+update_code_parser.add_argument("zip_file")
+
+def update_config(args):
+    update_args = dict(FunctionName=args.function_name)
+    if args.role:
+        update_args.update(Role=args.role)
+    if args.timeout:
+        update_args.update(Timeout=args.timeout)
+    if args.memory_size:
+        update_args.update(MemorySize=args.memory_size)
+    if args.environment:
+        cfg = getattr(clients, "lambda").get_function_configuration(FunctionName=args.function_name)
+        cfg["Environment"]["Variables"].update(args.environment)
+        update_args.update(Environment=cfg["Environment"])
+    return getattr(clients, "lambda").update_function_configuration(**update_args)
+
+def role_name_completer(**kwargs):
+    return [r.name for r in resources.iam.roles.all()]
+
+update_config_parser = register_parser(update_config, parent=lambda_parser, help="Update function configuration")
+update_config_parser.add_argument("function_name")
+update_config_parser.add_argument("--role", help="IAM role for the function").completer = role_name_completer
+update_config_parser.add_argument("--timeout", type=int,
+                                  help="The amount of time that Lambda allows a function to run before stopping it")
+update_config_parser.add_argument("--memory-size", type=int,
+                                  help="The amount of memory that your function has access to")
+update_config_parser.add_argument("--environment", nargs="+", metavar="NAME=VALUE", type=lambda x: x.split("=", 1),
+                                  help="Read environment variables for function, update with given values, write back")
