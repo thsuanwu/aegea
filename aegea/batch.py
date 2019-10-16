thsuanwu/aegea

commit b00296bcc88c837289ac26a30a257322f665b3f3

aegea batch: avoid setting resourceRequirements unless needed

aegea/batch.py
@@ -151,11 +151,11 @@ def ensure_job_definition(args):
     if args.ecs_image:
         args.image = get_ecr_image_uri(args.ecs_image)
     container_props = {k: getattr(args, k) for k in ("image", "vcpus", "memory", "privileged")}
-    container_props.update(volumes=[], mountPoints=[], resourceRequirements=[], environment=[], command=[])
+    container_props.update(volumes=[], mountPoints=[], environment=[], command=[])
     set_volumes(args, container_props)
     set_ulimits(args, container_props)
     if args.gpus:
-        container_props["resourceRequirements"].append({"type": "GPU", "value": str(args.gpus)})
+        container_props["resourceRequirements"] = [{"type": "GPU", "value": str(args.gpus)}]
     iam_role = ensure_iam_role(args.job_role, trust=["ecs-tasks"],
                                policies=["AmazonEC2FullAccess", "AmazonDynamoDBFullAccess", "AmazonS3FullAccess"])
     container_props.update(jobRoleArn=iam_role.arn)

commit f062bb214e72b79b5f05030a27cf6e91a8ca7f26

Fix job definition reuse regression introduced in b00296b

aegea/batch.py
@@ -157,7 +157,7 @@ def ensure_job_definition(args):
     if args.ecs_image:
         args.image = get_ecr_image_uri(args.ecs_image)
     container_props = {k: getattr(args, k) for k in ("image", "vcpus", "memory", "privileged")}
-    container_props.update(volumes=[], mountPoints=[], environment=[], command=[])
+    container_props.update(volumes=[], mountPoints=[], environment=[], command=[], resourceRequirements=[])
     set_volumes(args, container_props)
     set_ulimits(args, container_props)
     if args.gpus: