ada03862aa0ad3961396b5cf68f86ec7f806d202
Andrey Kislyuk <kislyuk@gmail.com>
2019-11-08 13:38:52 -0800

    aegea cost: add group by tag support

--- aegea/cost.py
+++ aegea/cost.py
@@ -22,11 +22,14 @@ def get_common_method_args(args):
                                 End=args.time_period_end.date().isoformat()))
 
 def cost(args):
-    get_cost_and_usage_args = dict(get_common_method_args(args),
-                                   Metrics=args.metrics,
-                                   GroupBy=[dict(Type="DIMENSION", Key=k) for k in args.group_by])
+    if not (args.group_by or args.group_by_tag):
+        args.group_by = ["SERVICE"]
+    get_cost_and_usage_args = dict(get_common_method_args(args), Metrics=args.metrics)
+    get_cost_and_usage_args["GroupBy"] = [dict(Type="DIMENSION", Key=k) for k in args.group_by]
+    get_cost_and_usage_args["GroupBy"] += [dict(Type="TAG", Key=k) for k in args.group_by_tag]
     rows = collections.defaultdict(dict)
-    title = "{} ({})".format(args.group_by[0], boto3.session.Session().profile_name)
+    title = "{} ({})".format(args.group_by[0] if args.group_by else "Tag:" + args.group_by_tag[0],
+                             boto3.session.Session().profile_name)
     args.columns, cell_transforms = [title], {"TOTAL": format_float}
     for page in clients.ce.get_cost_and_usage(**get_cost_and_usage_args)["ResultsByTime"]:
         args.columns.append(page["TimePeriod"]["Start"])
@@ -47,15 +50,18 @@ def cost(args):
 parser_cost = register_parser(cost, help="List AWS costs")
 parser_cost.add_argument("--time-period-start", type=Timestamp, default=Timestamp("-7d"),
                          help="Time to start cost history." + Timestamp.__doc__)
-parser_cost.add_argument("--time-period-end", type=Timestamp, default=Timestamp("-1d"),
+parser_cost.add_argument("--time-period-end", type=Timestamp, default=Timestamp("-0d"),
                          help="Time to end cost history." + Timestamp.__doc__)
 parser_cost.add_argument("--granularity", choices={"HOURLY", "DAILY", "MONTHLY"}, help="AWS cost granularity")
 parser_cost.add_argument("--metrics", nargs="+", default=["AmortizedCost"],
                          choices={"AmortizedCost", "BlendedCost", "NetAmortizedCost", "NetUnblendedCost",
                                   "NormalizedUsageAmount", "UnblendedCost", "UsageQuantity"})
-parser_cost.add_argument("--group-by", nargs="+", default=["SERVICE"],
-                         choices={"AZ", "INSTANCE_TYPE", "LEGAL_ENTITY_NAME", "LINKED_ACCOUNT", "OPERATION", "PLATFORM",
-                                  "PURCHASE_TYPE", "SERVICE", "TAGS", "TENANCY", "USAGE_TYPE", "REGION"})
+parser_cost.add_argument("--group-by", nargs="+", default=[],
+                         choices={"AZ", "INSTANCE_TYPE", "LINKED_ACCOUNT", "OPERATION", "PURCHASE_TYPE", "SERVICE",
+                                  "REGION", "USAGE_TYPE", "PLATFORM", "TENANCY", "RECORD_TYPE", "LEGAL_ENTITY_NAME",
+                                  "DEPLOYMENT_OPTION", "DATABASE_ENGINE", "CACHE_ENGINE", "INSTANCE_TYPE_FAMILY",
+                                  "BILLING_ENTITY", "RESERVATION_ID", "SAVINGS_PLANS_TYPE", "SAVINGS_PLAN_ARN"})
+parser_cost.add_argument("--group-by-tag", nargs="+", default=[])
 parser_cost.add_argument("--min-total", type=int, help="Omit rows that total below this number")
 
 def cost_forecast(args):
