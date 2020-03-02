f9162203910d616ace5338da969f095c1d471e30
Andrey Kislyuk <kislyuk@gmail.com>
2020-03-02 13:51:42 -0800

    Fixup for 3b43abdf558cc700dc35218190c54a477783a275

--- aegea/sfn.py
+++ aegea/sfn.py
@@ -38,8 +38,8 @@ parser.add_argument("state_machine").completer = complete_state_machine_name
 
 def describe(args):
     exec_desc = clients.stepfunctions.describe_execution(executionArn=args.execution_arn)
-    exec_desc["input"] = json.loads(exec_desc.get("input", {}))
-    exec_desc["output"] = json.loads(exec_desc.get("output", {}))
+    exec_desc["input"] = json.loads(exec_desc.get("input", "null"))
+    exec_desc["output"] = json.loads(exec_desc.get("output", "null"))
     return exec_desc
 
 parser = register_parser(describe, parent=sfn_parser, help="Describe an execution of a state machine")
